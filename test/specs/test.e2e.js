3b86d33d3c91c0ec3403b49fe92f82f342c4169d
maxwellHilmo <maxwellhilmo@gmail.com>
2024-11-12 08:28:27 -0700

    third try at pushing to repo.

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,10 @@
+import LoginPage from '../pageobjects/loginCode.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('standard_user', 'secret_sauce!')
+    })
+})
+
